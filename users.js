c8c78bec05eca3e7ded43433e39ab9e8916dd0a8
Alberto Sandoval <albertosand8@gmail.com>
2021-02-20 15:57:24 -0600

    Adding some files needed it for the practice, like the animals data inside a json file, a users and animals javascript files and a gitignore

---
+++ users.js
@@ -0,0 +1,11 @@
+const users = [{id: 1, name: 'User 1', age:30},
+{id: 2, name: 'User 2', age:40},
+{id: 3, name: 'User 3', age:24},
+{id: 4, name: 'User 4', age:100},
+{id: 5, name: 'User 5', age:90}
+];
+
+function create(name, age){
+    const id = user.lenght +1
+    users.push({id, name: '', age: ''})
+}
